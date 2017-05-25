5c9640417bf6cc68bf87c1f36f523251540bbc4d
jlugogar - Jose Lugo <jalugogarcia@gmail.com>
2017-05-25 09:49:12 -0400

    Create data classes.

---
+++ DataAccessLayer/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    class Person
+    {
+        private string id;
+        private string firstName;
+        private string lastName;
+        private string address;
+
+        public Person(string id, string fn, string ln, string addrs)
+        {
+            this.id = id;
+            firstName = fn;
+            lastName = ln;
+            address = addrs;
+        }
+
+    }
+}
